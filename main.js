9b766fb9fd4eb4a4c5a131b1e8a5f2ee8a29c89a
Josh <josh2507@hotmail.co.uk>
2024-08-09 09:39:31 +0100

    working word count in js

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-const countLetters = word => {
-//create the object which will store the occurrences
-    const count = {};
-}
+// const countLetters = word => {
+// //create the object which will store the occurrences
+//     const count = {};
+// }
 
-//loop over the letters of the word
-for (let i = 0; i < word.length; i++) {
-    const letter = word[i]
+function wordLength(word) {
+	let counter = 0; for (let i = 0; i < word.length; i++) {
+		counter++;
+	}
+	return counter;
 }
-
-		// if the property isn't in the object, it means that the letter is a new one and we'll create this property and assign it the value 1
-        if (!count[letter]) {
-			count[letter] = 1;
-
-			// else, it means that the property is already there and we just increment it by 1
-		} else {
-			count[letter]++;
-		}
-	      {return count;
-}
-
-export function sum(a, b) {
-    return a + b
-}
+console.log(wordLength("Coding"));
